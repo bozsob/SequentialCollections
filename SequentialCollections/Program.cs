abd23e7eee9d02733d8f6246bf6e85d8c05487f0
bozsob <bozso.beatrix@gmail.com>
2017-06-27 13:06:27 +0200

    LIFO - working with Stack

--- SequentialCollections/Program.cs
+++ SequentialCollections/Program.cs
@@ -17,6 +17,12 @@ namespace SequentialCollections
             queue.Enqueue("Third");
             queue.Enqueue("Fourth");
 
+            Console.WriteLine("My queue: ");
+            foreach(var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
             Console.WriteLine("FIFO (First-in-first-out)");
             while (queue.Count > 0)
             {
@@ -34,10 +40,42 @@ namespace SequentialCollections
                 }
                 
             }
-
-
-
             
+
+            Console.WriteLine("My stack: ");
+
+            Stack stack = new Stack();
+            Console.WriteLine("Push a new item (aaa) into stack.");
+            stack.Push("aaa");
+            Console.WriteLine("Push a new item (bbb) into stack.");
+            stack.Push("bbb");
+            Console.WriteLine("Push a new item (ccc) into stack.");
+            stack.Push("ccc");
+            Console.WriteLine("Push a new item (ddd) into stack.");
+            stack.Push("ddd");
+
+            Console.WriteLine("Now the stack looks like: ");
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("LIFO (Last-in-first-out)");
+            while (stack.Count > 0)
+            {
+                try
+                {
+                    object obj = stack.Pop();
+                    Console.WriteLine("From Stack: {0}", obj);
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error occured.");
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
     }
 }
