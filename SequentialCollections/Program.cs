68694f0690481cc9693e847c72eab22c11bd8208
bozsob <bozso.beatrix@gmail.com>
2017-06-27 14:03:39 +0200

    Hashtable - lookup table

--- SequentialCollections/Program.cs
+++ SequentialCollections/Program.cs
@@ -76,6 +76,36 @@ namespace SequentialCollections
                     Console.WriteLine(e.Message);
                 }
             }
+
+
+            Console.WriteLine("Hashtable..");
+            Hashtable lookup = new Hashtable();
+            lookup["0"] = "Zero";
+            lookup["1"] = "One";
+            lookup["2"] = "Two";
+
+            lookup.Add("3", "Three");
+            lookup.Add("4", "Four");
+            lookup.Add("5", "Five");
+
+            lookup["6"] = "Six";
+            lookup["7"] = "Seven";
+            lookup["8"] = "Eight";
+            lookup["9"] = "Nine";
+
+            string ourNumber = "888-555-1212";
+            Console.WriteLine("Our number is: " + ourNumber);
+            Console.WriteLine("The lookup..");
+
+            foreach (char c in ourNumber)
+            {
+                string digit = c.ToString();
+                if (lookup.ContainsKey(digit))
+                {
+                    Console.WriteLine(lookup[digit]);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
